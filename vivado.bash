f951caba283929384b41568e79149d17f4702fb3
nic-str <3697620+nic-str@users.noreply.github.com>
2023-09-18 17:04:04 +0200

    Added scripts to source env and start vivado

---
+++ vivado.bash
@@ -0,0 +1,5 @@
+call C:\Xilinx\Vivado\2022.1\settings64.bat
+
+mkdir log
+
+vivado -mode batch -source make.tcl -log log/vivado.log -journal log/vivado.jou
---
+++ vivado.sh
@@ -0,0 +1,5 @@
+source /tools/Xilinx/Vivado/2022.1/settings64.sh
+
+mkdir log
+
+vivado -mode batch -source make.tcl -log log/vivado.log -journal log/vivado.jou
